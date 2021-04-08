cc5e2ac8d97da2b35e70d5ae74ff6dfd3353537e
Oleksii Trekhleb <trehleb@gmail.com>
2021-04-08 18:56:01 +0200

    Do smth.

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,3 +1,3 @@
-export const gitHubRepoURL = 'https://github.com/trekhleb/js-seam-carver';
+export const gitHubRepoURL = 'https://github.com/trekhleb/js-image-carver';
 export const seamCarvingPaperURL = 'https://perso.crans.org/frenoy/matlab2012/seamcarving.pdf';
 export const seamCarvingBlogPostURL = 'https://trekhleb.dev/blog/2021/content-aware-image-resizing-in-javascript/';
